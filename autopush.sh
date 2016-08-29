d075262ef6760d862d410f2a9154efd32581a44c
zengjf <zengjf42@163.com>
2016-08-29 17:02:57 +0800

    修改autopush.sh文件。

--- autopush.sh
+++ autopush.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+message=$1
+gitRepository="git@github.com:AplexOS/LoveLife"
+
 if [ $# -ne 1 ];then
-	echo UAGE:
+	echo USAGE:
 	echo "   ./autopush.sh <message>"
 	exit -1
 fi
 
-message=$1
-gitRepository="git@github.com:AplexOS/LoveLife"
+if [ $message == "rm" ]; then
 
-git rm $(git ls-files --deleted)
+    deleteFile=$(git ls-files --deleted)
+    if [ ! -n  deleteFile ]; then
+        git rm deleteFile
+    fi
+
+    # git rm $(git ls-files --deleted)
+    echo "deleted repository."
+    exit 0
+fi
+
+if [ $message == "push" ]; then
+    git pull "$gitRepository" && git push "$gitRepository"
+
+    echo "push to repository."
+    exit 0
+fi
 
 # commit to repository and push to github
 git add . && git commit -m "$message" && git pull "$gitRepository" && git push "$gitRepository"
