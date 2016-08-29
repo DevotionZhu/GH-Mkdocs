7d7b1fec5ccfd98272c0c767a9057a7d7fdffb74
zengjf <zengjf42@163.com>
2016-08-29 16:28:49 +0800

    删除log相关内容。

--- autopush.sh
+++ autopush.sh
@@ -9,5 +9,7 @@ fi
 message=$1
 gitRepository="git@github.com:AplexOS/LoveLife"
 
+git rm $(git ls-files --deleted)
+
 # commit to repository and push to github
 git add . && git commit -m "$message" && git pull "$gitRepository" && git push "$gitRepository"
