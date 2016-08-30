1d3d9704f95d2a02fe01ee1b856593491278dac6
zengjf <zengjf42@163.com>
2016-08-31 07:34:23 +0800

    修改autorun.sh文件，重启web服务的时候能够自动杀死之前的进程。

--- LoveLife/autorun.sh
+++ LoveLife/autorun.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-mkdocs serve --dev-addr=127.0.0.1:9000 &
+ip=127.0.0.1
+port=9000
+
+getPid()
+{
+    pid=`ps aux | grep /usr/local/bin/mkdocs | head -n1 | awk '{print $2}'`
+}
+
+if [ ! -z $pid ];then
+    kill -9 $pid
+    echo "Have kill process $pid"
+fi
+
+mkdocs serve --dev-addr=$ip:$port &
+echo "mkdocs server has start."
